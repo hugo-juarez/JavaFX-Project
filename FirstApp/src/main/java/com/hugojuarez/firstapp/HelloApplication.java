76401607636941ddf381e907487abee8a4732abf
Hugo Juarez <hsebastianjuarezp@gmail.com>
2026-04-19 12:43:15 -0500

    feat: Testing lifecycle of JavaFX app

--- FirstApp/src/main/java/com/hugojuarez/firstapp/HelloApplication.java
+++ FirstApp/src/main/java/com/hugojuarez/firstapp/HelloApplication.java
@@ -10,8 +10,19 @@ import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 public class HelloApplication extends Application {
+
+    @Override
+    public void init() throws Exception {
+        System.out.println("Before the App starts");
+        System.out.println(Thread.currentThread().getName());
+
+        Thread.sleep(5000);
+    }
+
     @Override
     public void start(Stage stage) {
+        System.out.println(Thread.currentThread().getName());
+
         VBox root = new VBox();
 
         // Center elements
@@ -23,12 +34,21 @@ public class HelloApplication extends Application {
 
         Label label = new Label();
         Button button = new Button("Click Me!");
+        Button button1 = new Button("Block");
 
         // Adding button action
         button.setOnAction(e -> label.setText("Welcome to JavaFX Application!"));
+        button1.setOnAction(e -> {
+            System.out.println("UI now blocked");
+            try {
+                Thread.sleep(10000);
+            } catch (InterruptedException ex) {
+                ex.printStackTrace();
+            }
+        });
 
         // Adding to a layout
-        root.getChildren().addAll(label, button);
+        root.getChildren().addAll(label, button, button1);
 
         // Create a scene and add it to stage
         Scene scene = new Scene(root, 350, 300);
