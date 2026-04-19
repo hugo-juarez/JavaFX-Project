0df8e4ab149db91cb779aee40dee96d476f9d147
Hugo Juarez <hsebastianjuarezp@gmail.com>
2026-04-18 23:00:00 -0500

    feat: Created window application through Java no xml

--- FirstApp/src/main/java/com/hugojuarez/firstapp/HelloApplication.java
+++ FirstApp/src/main/java/com/hugojuarez/firstapp/HelloApplication.java
@@ -1,3 +1,41 @@
 package com.hugojuarez.firstapp;
 
+import javafx.application.Application;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
 
+public class HelloApplication extends Application {
+    @Override
+    public void start(Stage stage) {
+        VBox root = new VBox();
+
+        // Center elements
+        root.setAlignment(Pos.CENTER);
+
+        // Adding padding & spacing
+        root.setPadding(new Insets(20, 20, 20, 20));
+        root.setSpacing(20);
+
+        Label label = new Label();
+        Button button = new Button("Click Me!");
+
+        // Adding button action
+        button.setOnAction(e -> label.setText("Welcome to JavaFX Application!"));
+
+        // Adding to a layout
+        root.getChildren().addAll(label, button);
+
+        // Create a scene and add it to stage
+        Scene scene = new Scene(root, 350, 300);
+        stage.setScene(scene);
+
+        // Show window
+        stage.setTitle("JavaFX Application");
+        stage.show();
+    }
+}
