6a16eaeddc2bf74d19fab5d83078f8fd80c12591
Hugo Juarez <hsebastianjuarezp@gmail.com>
2026-04-24 14:24:46 -0500

    chore: Initialized concurrency project

---
+++ concurrency/src/main/java/com/hugojuarez/App.java
@@ -0,0 +1,43 @@
+package com.hugojuarez;
+
+import javafx.application.Application;
+import javafx.application.Platform;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+
+
+/**
+ * JavaFX App
+ */
+public class App extends Application {
+
+    @Override
+    public void start(Stage stage) {
+        VBox root = new VBox();
+        root.setAlignment(Pos.CENTER);
+        root.setSpacing(10);
+        Button stopUI = new Button("Stop UI");
+        Button exit = new Button("Exit");
+        stopUI.setOnAction(e -> {
+            try {
+                Thread.sleep(10000);
+            } catch (InterruptedException ex) {
+                ex.printStackTrace();
+            }
+        });
+        exit.setOnAction(e -> Platform.exit());
+        root.getChildren().addAll(stopUI, exit);
+        Scene scene = new Scene(root, 500, 450);
+        stage.setScene(scene);
+        stage.setTitle("Concurrency");
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch();
+    }
+
+}
